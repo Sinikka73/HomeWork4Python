386db521006aa210b467c9e0b4e62808b1d15d32
olga <bend_sinister@mail.ru>
2022-12-19 19:25:47 +0300

    t2

--- task1.py
+++ task1.py
@@ -9,11 +9,27 @@
 
 from sys import argv
 
+
+def salary_func(arg_b, arg_c, arg_d):
+    """
+    функция расчёта з.п
+    :param arg_b: выработка в часах
+    :param arg_c: ставка в час
+    :param arg_d: премия
+    """
+
+    print(f'выработка в часах: {arg_b} ')
+    print(f'ставка в чaс, руб: {arg_c} ')
+    print(f'премия, руб: {arg_d} ')
+    print(f'заработная плата: {int(arg_b) * int(arg_c) + int(arg_d)} '
+          f'руб')
+
+
 script_name, work_time, hourly_rate, premium = argv
-print("Имя скрипта", script_name )
-print(f'выработка в часах: {work_time} ')
-print(f'ставка в чaс, руб: {hourly_rate} ')
-print(f'премия, руб: {premium} ')
-print(f'заработная плата: {int(work_time) * int(hourly_rate) + int(premium)} '
-      f'руб')
+print("Имя скрипта", script_name)
+try:
+    salary_func(work_time, hourly_rate, premium)
+except ValueError:
+    print("Вы ввели недостаточно данных")
+
 
